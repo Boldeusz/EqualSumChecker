bfe756467b8f427f7ebe3574d90903dbb9984499
Boldeusz <mnpower@poczta.onet.pl>
2021-01-17 18:41:14 +0100

    added code

--- src/EqualSumChecker.java
+++ src/EqualSumChecker.java
@@ -1,2 +1,9 @@
 public class EqualSumChecker {
+    public static boolean hasEqualSum(int one, int two, int three){
+        int sum = one + two;
+        if (sum == three){
+            return true;
+        }
+        return false;
+    }
 }
